d375bffe0272cb999a040ef47c1a748dae8d7f12
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2017-02-11 18:15:22 +0100

    Added CSS

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ $.getJSON( "data.json", function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     $.each( data.colors, function( key, val ) {
-      items.push( "<span id='" + key + "'>" + ' ' + '<div style="padding: 1em; height: 3em; background: '+ this.hex +'">' + '</div><div style="padding: 1em;">' + this.color + '<br>' + this.hex + '</div></span>' );
+      items.push( "<span class='color' id='" + key + "'>" + ' ' + '<div class="color__block" style="background: '+ this.hex +'">' + '</div><div class="color__name"><strong>' + this.color + '</strong><br>' + this.hex + '</div></span>' );
     });
   });
 

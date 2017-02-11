e059832ff8e61329a8ef7d691394f3a2d6d0a780
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2017-02-11 18:54:35 +0100

    js fix

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 $.getJSON( "data.json", function( data ) {
   var items = [];
-  $.each( data, function( key, val ) {
-    $.each( data.colors, function( key, val ) {
-      items.push( "<span class='color' id='" + key + "'>" + ' ' + '<div class="color__block" style="background: '+ this.hex +'">' + '</div><div class="color__name"><strong>' + this.color + '</strong><br>' + this.hex + '</div></span>' );
-    });
+  $.each( data.colors, function( key, val ) {
+    items.push( "<span class='color' id='" + key + "'>" + ' ' + '<div class="color__block" style="background: '+ this.hex +'">' + '</div><div class="color__name"><strong>' + this.color + '</strong><br>' + this.hex + '</div></span>' );
   });
 
   $( "<div/>", {

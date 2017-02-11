c5192f036e0e3bd81af6f5e39181ca6e039363e7
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2017-02-11 18:06:14 +0100

    misc

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ $.getJSON( "data.json", function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     $.each( data.colors, function( key, val ) {
-      items.push( "<span style='width: 15%; font-size: .8em; margin: .5em; background: #efefef; padding-bottom: 1em; font-family: Helvetica; display: inline-block;' id='" + key + "'>" + ' ' + '<div style="padding: 1em; height: 3em; background: '+ this.hex +'">' + '</div><div style="padding: 1em;">' + this.color + '<br>' + this.hex + '</div></span>' );
+      items.push( "<span id='" + key + "'>" + ' ' + '<div style="padding: 1em; height: 3em; background: '+ this.hex +'">' + '</div><div style="padding: 1em;">' + this.color + '<br>' + this.hex + '</div></span>' );
     });
   });
 

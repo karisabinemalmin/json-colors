fd80590312cbc3f4d0ee1e737bf0c0b70b27e991
Kari Sabine Malmin <kari.malmin@makingwaves.com>
2017-03-06 09:46:34 +0100

    Misc

--- javascript.js
+++ javascript.js
@@ -1,14 +1,9 @@
 $.getJSON( "data.json", function( data ) {
   var items = [];
-<<<<<<< HEAD
-  $.each( data.colors, function( key, val ) {
-    items.push( "<span class='color' id='" + key + "'>" + ' ' + '<div class="color__block" style="background: '+ this.hex +'">' + '</div><div class="color__name"><strong>' + this.color + '</strong><br>' + this.hex + '</div></span>' );
-=======
   $.each( data, function( key, val ) {
     $.each( data.colors, function( key, val ) {
       items.push( "<span class='color' id='" + key + "'>" + ' ' + '<div class="color__block" style="background: '+ this.hex +'">' + '</div><div class="color__name"><strong>' + this.color + '</strong><br>' + this.hex + '</div></span>' );
     });
->>>>>>> parent of aab0281... fix
   });
 
   $( "<div/>", {
